1dca6694960547e87e31c5dd30981ff3cb6bdee1
gablum <gablum>
2018-05-29 15:28:17 +0200

    as data frame

--- attempt merging.R
+++ attempt merging.R
@@ -1,4 +1,4 @@
 ndata <- read.csv("2016_neighbourhood_profiles.csv")
 aggregated <- read.csv("aggregated.csv")
 ndatasmall <- ndata[,-c(1:3,5)]
-ndatasmall <- t(ndatasmall)
+ndatasmall <- as.data.frame(t(ndatasmall))
